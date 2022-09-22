4da114e3fdedd9f9d91d29eb8c6c8cc8793f72a6
Sazzad Rabbi <59183123+Sazzadrabbi@users.noreply.github.com>
2022-09-22 02:54:31 -0600

    Update alldata.js

--- src/data/alldata.js
+++ src/data/alldata.js
@@ -1,6 +1,6 @@
-export const teacherData = ["md Harun", "md Nowshed"];
-export const batchData = [2019, 2020];
-export const courseData = ["CSE", "EEE"];
+export const teacherData = ["Md Harun", "Md Nowshed", "Nusrat Jahan Farin", "Adnan Ferdeous Ashrafi"];
+export const batchData = [60,61,62,63,64,67,68, 69,70,71,72,73];
+export const courseData = ["CSE", "EEE", "BBA"];
 export const semisterData = [1, 2, 3, 4, 5, 6, 7, 8];
 
-export const positionData = ["profesor", "lecturer", "head", "General"];
+export const positionData = ["Profesor", "Lecturer", "Head", "General"];
